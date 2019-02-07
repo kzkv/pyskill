b82369c40166969e56f97754fdcd1ce1129c3adf
Tom Kazakov <tyoma@kzkv.ru>
2019-02-07 16:55:21 -0500

    q2 test

--- test_skills.py
+++ test_skills.py
@@ -9,21 +9,37 @@ import workfile
 ERR_INPUT = "err"
 
 
-# Q1: combine two numbers from a string
+# Q1: combine two ints from a string
+
+# function declaration, input and output
+# string manipulation
+# str -> int coercion
+# try/except or if/else wrapping
+
 @pytest.mark.q1
 def test_q1():
-    Q1 = [("5 6", 11), ("1 20", 21), ("9", ERR_INPUT), ("5+6", ERR_INPUT)]
+    Q1 = [("5 6", 11), ("1 20", 21), ("5.0 5", ERR_INPUT), ("9", ERR_INPUT), ("5+6", ERR_INPUT)]
 
     for pair in Q1:
         print("input is '{}', with exected result {}".format(pair[0], pair[1]))
         assert workfile.q1(pair[0]) == pair[1]
 
 
-# Q2: some other task for marker demonstration
+# Q2: read a JSON from file and manipulate the dict
+
+# file reading
+# closing file after use
+# JSON to dict
+# addressing dict entry by key
+# sorting by value
+# comparison token (==)
+
 @pytest.mark.q2
 def test_q2():
-    Q1 = [("5 6", 11), ("1 20", 21), ("9", ERR_INPUT), ("5+6", ERR_INPUT)]
+    Q2 = [("q2_1.json", ([('tree', 1001), ('two', 202), ('one', 5)], 'one', 202)),
+          ("q2_2.json", "err"),
+          ("no_such_file.json", "file_err")]
 
-    for pair in Q1:
-        print("input is '{}', with exected result {}".format(pair[0], pair[1]))
+    for pair in Q2:
+        print("expected output is '{}' for the file '{}'".format(pair[1], pair[0]))
         assert workfile.q2(pair[0]) == pair[1]
--- workfile.py
+++ workfile.py
@@ -1,16 +1,51 @@
 """ Workfile to be changed by the applicant """
 
+import json
+
+
 # Q1
-# Add two numbers, which come as white-space separated tokens in a string
-# Code fragment should be a function named q1, with sting as an input and sum returned
-# Any error should return string "err"
+# Add two ints, which come as white-space separated tokens in a string.
+
+# Code fragment should be a function named q1, with sting as an input and sum returned.
+# Any error should return string "err".
 
 
 def q1(string_):
-    return string_.split(" ")[0] + string_.split(" ")[1]
+    try:
+        return int(string_.split(" ")[0]) + int(string_.split(" ")[1])
+    except:
+        return "err"
 
-# def q1(string_):
-#     try:
-#         return int(string_.split(" ")[0]) + int(string_.split(" ")[1])
-#     except:
-#         return "err"
+
+# Q2
+# Read a JSON from file, put into a dict, return
+#   1) list of key-value tuples sorted by values (descending)
+#   2) key of a pair which has a value of 5
+#   3) value of a dict entry with key "two"
+
+# Code fragment should be a function named q2, with string (filename) as an input and tuple (dict, int) returned.
+# File-related errors should return "file_err", any other errors - "err"
+
+# JSON in the input is supposed to be like this: {"key1": 1, "key": 2}
+
+
+def q2(file_name):
+    try:
+        with open(file_name, "r") as file:
+            try:
+                json_data = json.loads(file.read())
+
+                for item in json_data.items():
+                    if item[1] == 5:
+                        return sorted(json_data.items(), key=lambda item: item[1], reverse=True), \
+                               item[0], \
+                               json_data["two"]
+
+            except:
+                return "err"
+
+    except:
+        return "file_err"
+
+
+print(q2("q2_1.json"))
